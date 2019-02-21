d5edd465ad5828c75fd0f0c1f7152f34066e15a3
付义兵 <websearch@163.com>
2019-02-21 10:56:20 +0800

    reset redis configurations

--- src/RedisServiceProvider.php
+++ src/RedisServiceProvider.php
@@ -1,41 +1,43 @@
 <?php
-
 namespace Uniondrug\Redis;
 
 use Phalcon\Config;
 use Phalcon\Di\ServiceProviderInterface;
 
+/**
+ * @package Uniondrug\Redis
+ */
 class RedisServiceProvider implements ServiceProviderInterface
 {
+    /**
+     * @param \Phalcon\DiInterface $di
+     */
     public function register(\Phalcon\DiInterface $di)
     {
-        $di->set(
-            'redis',
-            function () {
-                if (!extension_loaded('redis')) {
-                    throw new \RuntimeException("Extension redis MUST be installed and loaded");
-                }
-
-                $options = config()->path('redis.options');
-                if (!$options instanceof Config) {
-                    throw new \RuntimeException("Redis options cannot be empty");
-                }
-
-                return new Client($options->toArray());
+        /**
+         * 调整配置兼容
+         * @author wsfuyibing <webserach@163.com>
+         * @date   2019-02-21
+         */
+        $config = config()->path('redis');
+        // 1. Redis对象
+        $optConfig = isset($config->options) && $config->options instanceof Config ? $config->options->toArray() : [];
+        $di->set('redis', function() use ($optConfig){
+            if (!extension_loaded('redis')) {
+                throw new \RuntimeException("Extension redis MUST be installed and loaded");
             }
-        );
-
-        $di->setShared(
-            'redisLock',
-            function () {
-                $redis = app()->getShared('redis');
-                if (!$redis) {
-                    throw new \RuntimeException("Cannot get redis instance");
-                }
-                $retryDelay = config()->path('redis.lock.retryDelay', 200);
-                $retryCount = config()->path('redis.lock.retryCount', 3);
-                return new RedisLock([$redis], $retryDelay, $retryCount);
+            return new Client($optConfig);
+        });
+        // 2. Redis锁
+        $lockConfig = isset($config->lock) && $config->lock instanceof Config ? $config->lock->toArray() : [];
+        $di->setShared('redisLock', function() use ($lockConfig){
+            $redis = app()->getShared('redis');
+            if (!$redis) {
+                throw new \RuntimeException("Cannot get redis instance");
             }
-        );
+            $retryDelay = isset($lockConfig['retryDelay']) ? (int) $lockConfig['retryDelay'] : 200;
+            $retryCount = isset($lockConfig['retryCount']) ? (int) $lockConfig['retryCount'] : 3;
+            return new RedisLock([$redis], $retryDelay, $retryCount);
+        });
     }
 }
